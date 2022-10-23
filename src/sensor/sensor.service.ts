965373946e5887a6adee741f5d2dce6fccde3f7a
emrecelik <emrecelikdev@gmail.com>
2022-10-23 15:06:38 +0300

    DF-0001 -DTO and Entity refactor

--- src/sensor/sensor.service.ts
+++ src/sensor/sensor.service.ts
@@ -1,20 +1,20 @@
-import { Body, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { SensorEntity } from './sensor.entity';
+import { Sensor } from './sensor.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class SensorService {
   constructor(
-    @InjectRepository(SensorEntity)
-    private sensorRepository: Repository<SensorEntity>,
+    @InjectRepository(Sensor)
+    private sensorRepository: Repository<Sensor>,
   ) {}
 
-  getAll(): Promise<SensorEntity[]> {
+  getAll(): Promise<Sensor[]> {
     return this.sensorRepository.find();
   }
 
-  createOne(sensor: SensorEntity): Promise<SensorEntity> {
+  createOne(sensor: Sensor): Promise<Sensor> {
     const data = this.sensorRepository.create(sensor);
     return this.sensorRepository.save(data);
   }
