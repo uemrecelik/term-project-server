64c91610ae222811648645ff5f3b6ab611448b45
GGizer99 <gizerdogan@hotmail.com>
2022-12-16 12:53:55 +0300

    DF-0011 - Unit Tests

--- src/sensor/sensor.controller.spec.ts
+++ src/sensor/sensor.controller.spec.ts
@@ -16,3 +16,26 @@ describe('SensorController', () => {
     expect(controller).toBeDefined();
   });
 });
+
+describe('SensorController', () => {
+  let controller: SensorController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [SensorController],
+    }).compile();
+
+    describe('addData', () => {
+      // addData testing for adding new data
+      it('should create a Sensor object with the given temperature and humidity', () => {
+        const result = controller.addData(25, 60);
+        expect(result).toEqual({
+          id: null,
+          temperature: 25,
+          humidity: 60,
+          created_at: expect.any(Date),
+        });
+      });
+    });
+  });
+});
--- src/sensor/sensor.service.spec.ts
+++ src/sensor/sensor.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SensorService } from './sensor.service';
+import { Sensor } from './sensor.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 describe('SensorService', () => {
   let service: SensorService;
+  let repository: Repository<Sensor>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SensorService],
     }).compile();
 
+    repository = module.get<Repository<Sensor>>(getRepositoryToken(Sensor));
+
     service = module.get<SensorService>(SensorService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    //Testing getAll method
+    it('should return an array of sensors', async () => {
+      const result = [
+        { id: 1, temperature: 25, humidity: 50, created_at: new Date() },
+      ];
+      jest.spyOn(repository, 'find').mockResolvedValue(result);
+
+      expect(await service.getAll()).toEqual(result);
+    });
   });
 });
