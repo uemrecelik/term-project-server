d515f79bbe6e0197c9637ee279dbc353f15a3205
emrecelik <emrecelikdev@gmail.com>
2023-03-11 15:41:07 +0300

    DF-clear-emre - getPlantbyId added

--- src/sensor/sensor.controller.ts
+++ src/sensor/sensor.controller.ts
@@ -5,26 +5,31 @@ import { Sensor } from './sensor.entity';
 @Controller('sensor')
 export class SensorController {
   constructor(private readonly sensorService: SensorService) {}
+
   @Get('/all')
   getAll(): Promise<Sensor[]> {
-    //It receives the get requests from the server and runs the getAll method in the service class
-    // and sends all the data in the sensor entity
     return this.sensorService.getAll();
   }
+
   @Get()
   addData(
-    @Query('id') id: number,
+    @Query('id') plantId: number,
     @Query('temp') temp: number,
     @Query('hum') hum: number,
   ): any {
-    // Recives get requst on /sensor path and pare temp, hum values from url then creates an entity.
     const date: Date = new Date();
     const sensorData: Sensor = {
-      id: id,
+      id: null,
+      plantId: plantId,
       temperature: temp,
       humidity: hum,
       created_at: date,
     };
-    return this.sensorService.createOne(sensorData); //Create record to db from newly generated entity
+    return this.sensorService.createOne(sensorData);
+  }
+
+  @Get('/plants-sensor')
+  getPlantsSensor(@Query('id') plantId: number): Promise<Sensor[]> {
+    return this.sensorService.getPlantsSensor(plantId);
   }
 }
